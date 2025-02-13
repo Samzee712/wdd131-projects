e0b7372f5dc2ca63108847438d374d82e51159a1
Samze712 <samiwizee@gmail.com>
2025-02-13 16:08:05 +0100

    Deleted all files

--- Scripts/siteplan.js
+++
@@ -1,3 +0,0 @@
- // Dynamic Year and Last Modified
- document.getElementById("currentYear").textContent = new Date().getFullYear();
- document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
