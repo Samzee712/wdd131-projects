75bae600545759aa0772172638fc9f00db7c5703
Samze712 <samiwizee@gmail.com>
2025-02-13 14:24:40 +0100

    Initial comit

---
+++ Scripts/site-plan.js
@@ -0,0 +1,3 @@
+ // Dynamic Year & Last Modified
+ document.getElementById("currentYear").textContent = new Date().getFullYear();
+ document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
